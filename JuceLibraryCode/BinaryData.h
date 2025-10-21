063db65f28fdd3f7e7ca9a88c339d381e190ec49
sleach100 <sleach100@gmail.com>
2025-10-20 21:06:55 -0700

    Add Audio Samples

--- JuceLibraryCode/BinaryData.h
+++ JuceLibraryCode/BinaryData.h
@@ -8,6 +8,78 @@
 
 namespace BinaryData
 {
+    extern const char*   _808Kick_wav;
+    const int            _808Kick_wavSize = 146710;
+
+    extern const char*   _808Robotic_wav;
+    const int            _808Robotic_wavSize = 472678;
+
+    extern const char*   _808Vynlz_wav;
+    const int            _808Vynlz_wavSize = 581862;
+
+    extern const char*   ClapAqua_Clap_wav;
+    const int            ClapAqua_Clap_wavSize = 496926;
+
+    extern const char*   ClapDecontaminated_Clap_wav;
+    const int            ClapDecontaminated_Clap_wavSize = 421838;
+
+    extern const char*   ClapMotto_Clap_wav;
+    const int            ClapMotto_Clap_wavSize = 133030;
+
+    extern const char*   Closed_HatHi_Hat_Stove_wav;
+    const int            Closed_HatHi_Hat_Stove_wavSize = 12402;
+
+    extern const char*   Closed_HatAdventure_Hat_WAV;
+    const int            Closed_HatAdventure_Hat_WAVSize = 26106;
+
+    extern const char*   Closed_HatHat_wav;
+    const int            Closed_HatHat_wavSize = 87734;
+
+    extern const char*   Crashes__CymbalsCrash_1_wav;
+    const int            Crashes__CymbalsCrash_1_wavSize = 518222;
+
+    extern const char*   Crashes__CymbalsCrash_2_wav;
+    const int            Crashes__CymbalsCrash_2_wavSize = 280922;
+
+    extern const char*   Crashes__CymbalsCymbal_wav;
+    const int            Crashes__CymbalsCymbal_wavSize = 794456;
+
+    extern const char*   Open_HatFunkXelf_wav;
+    const int            Open_HatFunkXelf_wavSize = 606564;
+
+    extern const char*   Open_hatGilberto_Gil__Morena_wav;
+    const int            Open_hatGilberto_Gil__Morena_wavSize = 112292;
+
+    extern const char*   Open_hatMinako_Yoshida__Clouds_wav;
+    const int            Open_hatMinako_Yoshida__Clouds_wavSize = 265868;
+
+    extern const char*   PercussionConga_wav;
+    const int            PercussionConga_wavSize = 40034;
+
+    extern const char*   PercussionDrum_Beat_WAV;
+    const int            PercussionDrum_Beat_WAVSize = 125590;
+
+    extern const char*   PercussionMac_x_Pharrell_Bongo_wav;
+    const int            PercussionMac_x_Pharrell_Bongo_wavSize = 1057408;
+
+    extern const char*   SnareAlchemist_Snare_wav;
+    const int            SnareAlchemist_Snare_wavSize = 27226;
+
+    extern const char*   SnareDiplomatic_wav;
+    const int            SnareDiplomatic_wavSize = 19116;
+
+    extern const char*   SnareSnare_Box_wav;
+    const int            SnareSnare_Box_wavSize = 29216;
+
+    extern const char*   VOXGrunt_wav;
+    const int            VOXGrunt_wavSize = 168350;
+
+    extern const char*   VOXScream_wav;
+    const int            VOXScream_wavSize = 162860;
+
+    extern const char*   VOXYell_wav;
+    const int            VOXYell_wavSize = 217692;
+
     extern const char*   LonePearLogic_png;
     const int            LonePearLogic_pngSize = 331159;
 
@@ -18,7 +90,7 @@ namespace BinaryData
     const int            MuteON_pngSize = 9965;
 
     extern const char*   SlotMachineUserManual_html;
-    const int            SlotMachineUserManual_htmlSize = 488819;
+    const int            SlotMachineUserManual_htmlSize = 488854;
 
     extern const char*   SlotMachine_ico;
     const int            SlotMachine_icoSize = 4286;
@@ -33,7 +105,7 @@ namespace BinaryData
     const int            SoloON_pngSize = 9950;
 
     // Number of elements in the namedResourceList and originalFileNames arrays.
-    const int namedResourceListSize = 8;
+    const int namedResourceListSize = 32;
 
     // Points to the start of a list of resource names.
     extern const char* namedResourceList[];
